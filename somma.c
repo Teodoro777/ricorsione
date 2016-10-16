2be5b31c92b0e805128e0961cd625c4854ff674e
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-10-16 15:07:43 +0200

    suggerimento somma

--- somma.c
+++ somma.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
 /* Il programma esegue la somma tra due numeri 
- * tramite una funzione */
+ * tramite una funzione ricorsiva.
+ *
+ * Nota: b deve essere positivo */
 
 int somma(int a, int b);
 
 int main()
 {
-  int a, b;
+  int a, b = -1;
 
   printf("Inserisci il primo numero: ");
   scanf("%d", &a);
 
-  printf("Inserisci il secondo numero: ");
-  scanf("%d", &b);
+  do {
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+  } while (b < 0);
 
   printf("La somma e' %d\n", somma(a, b));
 
   return 0;
 }
 
-int somma (int a, int b) {
-  if (a == 0)
-    return b;
-  else if (b == 0)
-    return a;
-  else 
-    return a + b;
-}
 
+int somma (int a, int b)
+{
+  /* il caso base e' OK */
+  if (b == 0)
+    return a;
+
+  /* se b non e' zero bisogna ridurre il problema
+   * della somma di a e b a qualcosa di piu' semplice
+   */
+  return ... somma(...) ...;
+}
